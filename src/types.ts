6c8405a3c7e8054ddd050f2ccf495abee63a799a
Kent <Kent>
2025-05-16 09:19:15 -0400

    Split the OrbitPlot off into its own component set up passing of props added a type.ts file = import/export types now

---
+++ src/types.ts
@@ -0,0 +1,96 @@
+export interface NEO_JSON_Object {
+    id: number;
+    neo_reference_id: number;
+    name: string;
+    designation: string;
+    absolute_magnitude_h: number;
+    is_potentially_hazardous_asteroid: boolean;
+    nasa_jpl_url: string;
+    estimated_diameter: {
+      feet: {
+        estimated_diameter_max: number;
+        estimated_diameter_min: number;
+      };
+      kilometers: {
+        estimated_diameter_max: number;
+        estimated_diameter_min: number;
+      };
+      meters: {
+        estimated_diameter_max: number;
+        estimated_diameter_min: number;
+      };
+      miles: {
+        estimated_diameter_max: number;
+        estimated_diameter_min: number;
+      };
+    };
+    orbital_data: {
+      aphelion_distance: string;
+      ascending_node_longitude: string;
+      data_arc_in_days: number;
+      eccentricity: string;
+      epoch_osculation: string;
+      equinox: string;
+      first_observation_date: string;
+      inclination: string;
+      jupiter_tisserand_invariant: string;
+      last_observation_date: string;
+      mean_anomaly: string;
+      mean_motion: string;
+      minimum_orbit_intersection: string;
+      observations_used: number;
+      orbit_determination_date: string;
+      orbit_id: string;
+      orbit_uncertainty: string;
+      orbital_period: string;
+      perihelion_argument: string;
+      perihelion_distance: string;
+      perihelion_time: string;
+      semi_major_axis: string;
+      orbit_class: {
+        orbit_class_type: string;
+        orbit_class_description: string;
+        orbit_class_range: string;
+      };
+    };
+    close_approach_data: Array<Close_Approach_Data>;
+    links: {
+      self: string;
+    };
+  }
+export interface Close_Approach_Data {
+    close_approach_date: string;
+    close_approach_date_full: string;
+    epoch_date_close_approach: number;
+    orbiting_body: string;
+    miss_distance: {
+      astronomical: string;
+      lunar: string;
+      kilometers: string;
+      miles: string;
+    };
+    relative_velocity: {
+      kilometers_per_hour: string;
+      kilometers_per_second: string;
+      miles_per_hour: string;
+    };
+  }
+export interface OrbitXYZ {
+    x: Array<number>;
+    y: Array<number>;
+    z: Array<number>;
+  }
+export interface Orbital_Data {
+    date: string; //Date of data collection => used to calculate future values relative to a constant t (J2000)
+    M: number; //Mean Anomaly => How far around the orbit the object currently is in degrees. 0 at perihelion, 180 at aphelion
+    e: number; //Eccentricity => How 'pointy' the orbit is: 0 = circular, < 1 = Elliptical, > 1 == Parabolic, > 1 = Hyperbolic
+    q: number; //Perihelion distance => Distance in A.U. from the sun at closest part of orbit
+    o: number; //'Omega'(Capital) or ascending node longitude => Angle at the point where the orbit ascending passes through the horizontal plane of the earth and sun, in deg (counterclockwise)
+    i: number; //Inclination => // angle (deg) between the plane of the orbit and the reference plane > 0-90deg normal direction orbit > 90-180deg retrograde orbits
+    p: number; //'omega'(lowecase) or perihelion argument => The angle between the ascending node and the periapsis (lowest point of orbit)
+    v?: number; //True anomaly (DERIVED) => actual angle between the orbiting body and periapsis
+    a?: number; //Mean distance (DERIVED) => also known as the Semi-Major Axis
+    r?: number; //Heliocenteric Distance (DERIVED) => Body's distance ot the sun
+    orbit?: OrbitXYZ;
+}
+  
